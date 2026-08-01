5bd3c1638236d38aa7856c7220d7368accc80d40
Coding-BR <136135404+Coding-BR@users.noreply.github.com>
2026-08-01 13:49:41 -0300

    recon(zte_tpd): attest zlog registration callback

--- kernel_development/drivers/reconstructed/zte_tpd/zlog_register_work.c
+++ kernel_development/drivers/reconstructed/zte_tpd/zlog_register_work.c
@@ -5,6 +5,6 @@ void zlog_register_work(struct work_struct *work)
 	struct ztp_device *cdev = tpd_zlog_device();
 
 	(void)work;
-	if (cdev && !cdev->zlog_registered)
+	if (!cdev->zlog_registered)
 		tpd_zlog_register(cdev);
 }
