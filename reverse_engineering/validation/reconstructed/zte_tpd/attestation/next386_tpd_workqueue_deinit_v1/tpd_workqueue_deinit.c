88fc9cc85b453e09bdee4e7e136d1534542ff5a4
Coding-BR <Coding-BR@users.noreply.github.com>
2026-08-13 13:39:06 -0300

    attest tpd workqueue deinit semantic parity

---
+++ reverse_engineering/validation/reconstructed/zte_tpd/attestation/next386_tpd_workqueue_deinit_v1/tpd_workqueue_deinit.c
@@ -0,0 +1,19 @@
+void tpd_workqueue_deinit(void)
+{
+  __int64 v3; // x21
+  __int64 v6; // x20
+
+  v3 = tpd_cdev;
+  printk(unk_322AA, "tpd_workqueue_deinit");
+  tpd_report_work_deinit();
+  tpd_resume_work_deinit();
+  v6 = tpd_cdev;
+  printk(unk_322AA, "tpd_probe_work_deinit");
+  cancel_delayed_work_sync(v6 + 2256);
+  v6 = tpd_cdev;
+  printk(unk_322AA, "zlog_register_work_deinit");
+  cancel_delayed_work_sync(v6 + 2640);
+  vfree(*(_QWORD **)(v6 + 3048));
+  *(_QWORD *)(v6 + 3048) = 0;
+  cancel_delayed_work_sync(v3 + 2360);
+}
