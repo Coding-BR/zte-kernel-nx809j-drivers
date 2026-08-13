3eb643dbd8a090d9ad03086e6009568baef27c76
Coding-BR <Coding-BR@users.noreply.github.com>
2026-08-12 22:24:04 -0300

    fix and attest tpd get follow hand level

--- kernel_development/drivers/reconstructed/zte_tpd/tpd_get_follow_hand_level.c
+++ kernel_development/drivers/reconstructed/zte_tpd/tpd_get_follow_hand_level.c
@@ -3,7 +3,7 @@ int tpd_get_follow_hand_level(struct ztp_device *cdev)
   unsigned long a1 = (unsigned long)cdev;
   __int64 v1; // x9
 
-  v1 = *(_QWORD *)(a1 + 3072);
-  *(_DWORD *)(a1 + 1128) = *(_DWORD *)(v1 + 1536);
+  v1 = *(_QWORD *)(a1 + 0xdb8);
+  *(_DWORD *)(a1 + 0x468) = *(_DWORD *)(v1 + 0x600);
   return 0;
 }
