af466adc5f5d2170b9810f2d342d3d2fa7726d32
Coding-BR <Coding-BR@users.noreply.github.com>
2026-08-12 22:46:44 -0300

    fix and attest tpd set stability level

---
+++ kernel_development/drivers/validation/zte_tpd/host/tpd_set_stability_level_aarch64_assembly_input.c
@@ -0,0 +1,15 @@
+#define ZTE_TPD_AARCH64_INPUT 1
+typedef long long __int64;
+typedef unsigned long long _QWORD;
+typedef unsigned int _DWORD;
+typedef unsigned char _BYTE;
+struct syna_tcm;
+typedef struct ztp_device ztp_device;
+extern int printk(const char *format, ...);
+extern const char unk_34878[];
+extern const char unk_3C8D6[];
+extern const char unk_3B5E0[];
+extern int syna_dev_set_stability_level(struct syna_tcm *tcm,
+                                         int level,
+                                         int value);
+#include "../../../reconstructed/zte_tpd/tpd_set_stability_level.c"
