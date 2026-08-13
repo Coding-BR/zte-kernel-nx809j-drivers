5855214682715a386762eaddbfc9b380a6c31165
Coding-BR <Coding-BR@users.noreply.github.com>
2026-08-13 17:45:34 -0300

    attest rotation limit level semantic parity

--- kernel_development/drivers/reconstructed/zte_tpd/get_rotation_limit_level.c
+++ kernel_development/drivers/reconstructed/zte_tpd/get_rotation_limit_level.c
@@ -1,19 +1,22 @@
-ssize_t get_rotation_limit_level(struct file *file, char __user *buffer, size_t count, loff_t *offset)
+struct ztp_device;
+typedef int (*get_rotation_limit_level_callback_t)(struct ztp_device *cdev);
+
+ssize_t get_rotation_limit_level(struct file *file, char __user *buffer,
+                                 size_t count, loff_t *offset)
 {
   __int64 a2 = (__int64)buffer;
   __int64 a3 = (__int64)count;
   __int64 *a4 = (__int64 *)offset;
 
   (void)file;
-  __int64 result; // x0
-  __int64 v5; // x8
-  __int64 v8; // x22
-  void (__fastcall *v10)(_QWORD); // x8
-  __int64 v11; // x0
-  int v12; // w0
-  char s[32]; // [xsp+8h] [xbp-18h] BYREF
-  __int16 v14; // [xsp+10h] [xbp-10h]
-  __int64 v15; // [xsp+18h] [xbp-8h]
+  __int64 result;
+  __int64 v5;
+  __int64 v8;
+  get_rotation_limit_level_callback_t v10;
+  int v12;
+  char s[16];
+  __attribute__((unused)) __int16 v14;
+  __attribute__((unused)) __int64 v15;
 
   result = 0;
   v15 = *(_QWORD *)(_ReadStatusReg(SP_EL0) + 1808);
@@ -23,17 +26,15 @@ ssize_t get_rotation_limit_level(struct file *file, char __user *buffer, size_t 
   if ( !v5 )
   {
     v8 = tpd_cdev;
-    v10 = *(void (__fastcall **)(_QWORD))(tpd_cdev + 3280);
+    v10 = *(get_rotation_limit_level_callback_t *)(tpd_cdev + 0xe88);
     if ( v10 )
     {
-      v11 = tpd_cdev;
       /* CFI check removed */
-      v10(v11);
+      v10((struct ztp_device *)tpd_cdev);
     }
-    printk(unk_3A127, "get_rotation_limit_level", *(unsigned int *)(v8 + 12));
-    v12 = snprintf(s, 0xAu, "%u\n", *(_DWORD *)(v8 + 12));
+    printk(unk_3A127, "get_rotation_limit_level", *(unsigned int *)(v8 + 0xc));
+    v12 = snprintf(s, 0xa, "%u\n", *(unsigned int *)(v8 + 0xc));
     result = simple_read_from_buffer(a2, a3, a4, s, v12);
   }
-  _ReadStatusReg(SP_EL0);
   return result;
 }
