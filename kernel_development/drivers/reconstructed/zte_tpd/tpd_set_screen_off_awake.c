34547c448c7537d40464390078bb4fe03a09afbb
Coding-BR <Coding-BR@users.noreply.github.com>
2026-08-13 00:57:23 -0300

    attest tpd set screen off awake reconstruction

--- kernel_development/drivers/reconstructed/zte_tpd/tpd_set_screen_off_awake.c
+++ kernel_development/drivers/reconstructed/zte_tpd/tpd_set_screen_off_awake.c
@@ -4,7 +4,7 @@ int tpd_set_screen_off_awake(struct ztp_device *cdev, int a2)
   _DWORD *v3; // x8
   int v4; // w9
 
-  v3 = *(_DWORD **)(a1 + 3072);
+  v3 = *(_DWORD **)(a1 + 0xdb8);
   v4 = v3[351];
   v3[381] = a2;
   if ( v4 == 1 )
