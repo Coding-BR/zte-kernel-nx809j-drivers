e667804de85c178572633a4a2d32471da7cbbf9c
Coding-BR <Coding-BR@users.noreply.github.com>
2026-08-14 07:31:06 -0300

    attest syna pm suspend reconstruction

---
+++ reverse_engineering/validation/reconstructed/zte_tpd/attestation/next183_syna_pm_suspend_v1/candidate_source.c
@@ -0,0 +1,11 @@
+int syna_pm_suspend(struct device *dev)
+{
+  __int64 a1 = (__int64)dev;
+  __int64 v3; // x19
+
+  v3 = *(_QWORD *)(a1 + 152);
+  printk("\0016[info ] %s: system enters into pm_suspend", "syna_pm_suspend");
+  *(_BYTE *)(v3 + 1400) = 1;
+  *(_DWORD *)(v3 + 1368) = 0;
+  return 0;
+}
