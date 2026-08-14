226201ca90bf083be1a1ba60076bf8926656c2b1
Coding-BR <Coding-BR@users.noreply.github.com>
2026-08-14 13:26:42 -0300

    attest tp game partition write

--- kernel_development/drivers/reconstructed/zte_tpd/tp_game_partition_write.c
+++ kernel_development/drivers/reconstructed/zte_tpd/tp_game_partition_write.c
@@ -1,41 +1,66 @@
 ssize_t tp_game_partition_write(struct file *file, const char __user *buffer, size_t count, loff_t *offset)
 {
-  __int64 a2 = (__int64)buffer;
-  __int64 a3 = (__int64)count;
+#if defined(__aarch64__)
+  register const char __user *a2 __asm__("x21") = buffer;
+  register size_t a3 __asm__("x19") = count;
+  register __int64 v5 __asm__("x20") = tpd_cdev;
+#else
+  const char __user *a2 = buffer;
+  size_t a3 = count;
+  __int64 v5 = tpd_cdev;
+#endif
 
   (void)file;
   (void)offset;
-  __int64 v5; // x20
-  __int64 v6; // x2
-  void *v7; // x0
   void (__fastcall *v8)(__int64, char *); // x8
   char s[1024]; // [xsp+8h] [xbp-408h] BYREF
-  __int64 v11; // [xsp+408h] [xbp-8h]
 
-  v11 = *(_QWORD *)(_ReadStatusReg(SP_EL0) + 1808);
-  v5 = tpd_cdev;
-  printk(unk_39237, "tp_game_partition_write", 1024);
+  printk("\x01\x35tpd: %s:%lu, %zu.\n", "tp_game_partition_write", 1024UL, (size_t)a3);
   if ( a3 >= 0x401 )
   {
-    v7 = unk_3924C;
-LABEL_8:
-    printk(v7, "tp_game_partition_write", v6);
+    printk("\x01\x35tpd: %s:Input data too large.\n", "tp_game_partition_write");
     a3 = -22;
-    goto LABEL_9;
   }
-  memset(s, 0, sizeof(s));
-  _check_object_size(s, a3, 0);
-  if ( zte_inline_copy_from_user(s, (const void __user *)a2, a3) )
+  else
   {
-    v7 = unk_35311;
-    goto LABEL_8;
+    memset(s, 0, sizeof(s));
+    _check_object_size(s, a3, 0);
+    if ( zte_inline_copy_from_user(s, a2, a3) )
+    {
+      printk("\x01\x35tpd: %s:Failed to copy data from user space.\n", "tp_game_partition_write");
+      a3 = -22;
+    }
+    else
+    {
+      v8 = *(void (__fastcall **)(__int64, char *))(v5 + 3920);
+#if defined(__aarch64__)
+      __asm__ volatile(
+        "add x1, sp, #8\n"
+        "mov x0, %x[device]\n"
+        "ldur w16, [%x[callback], #-4]\n"
+        "movk w17, #0x5fb2\n"
+        "movk w17, #0x64f2, lsl #16\n"
+        "cmp w16, w17\n"
+        "b.eq 1f\n"
+        "brk #0x8228\n"
+        "1:\n"
+        "blr %x[callback]"
+        :
+        : [device] "r"(v5), [callback] "r"(v8)
+        : "x0", "x1", "x16", "x17", "cc", "memory");
+#else
+      if ( *((_DWORD *)v8 - 1) != 1693605810 ) {
+#if defined(ZTE_TPD_HOST_TEST)
+        __break(0x8228u);
+#else
+        __builtin_trap();
+#endif
+      }
+      v8(v5, s);
+#endif
+      snprintf((char *)(v5 + 65), 0x400u, "%s\n", s);
+      printk("\x01\x35tpd: %s:Received data: %s.\n", "tp_game_partition_write", (char *)(v5 + 65));
+    }
   }
-  v8 = *(void (__fastcall **)(__int64, char *))(v5 + 3480);
-  /* CFI check removed */
-  v8(v5, s);
-  snprintf((char *)(v5 + 65), 0x400u, "%s\n", s);
-  printk(unk_318B2, "tp_game_partition_write", v5 + 65);
-LABEL_9:
-  _ReadStatusReg(SP_EL0);
   return a3;
 }
