fc3b3213481e92bbdb3e288d8728f34b5d98ad9c
Coding-BR <Coding-BR@users.noreply.github.com>
2026-08-24 04:05:11 -0300

    attest tpd id7 report work exact

---
+++ reverse_engineering/validation/reconstructed/zte_tpd/attestation/next497_tpd_id7_report_work_exact_v1/joern_source_view.c
@@ -0,0 +1,5 @@
+void tpd_id7_report_work(struct work_struct *work)
+{
+  (void)work;
+  edge_long_press_up((struct input_dev *)qword_312E0, 7);
+}
---
+++ reverse_engineering/validation/reconstructed/zte_tpd/attestation/next497_tpd_id7_report_work_exact_v1/stock_decompiled/0111_0010fddc_tpd_id7_report_work.c
@@ -0,0 +1,8 @@
+
+void tpd_id7_report_work(void)
+
+{
+  edge_long_press_up(DAT_00131a60,7);
+  return;
+}
+
