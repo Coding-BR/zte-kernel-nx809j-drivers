2af26b1f37d924056a28700e31d4ed284aec77bd
Coding-BR <Coding-BR@users.noreply.github.com>
2026-08-27 15:32:58 -0300

    promote headset_state_show offline exact

---
+++ reverse_engineering/validation/reconstructed/zte_tpd/attestation/next407_headset_state_show_exact_v2/headset_state_show.c
@@ -0,0 +1,41 @@
+struct ztp_device;
+typedef int (*headset_state_show_callback_t)(struct ztp_device *cdev);
+
+ssize_t headset_state_show(struct file *file, char __user *buffer, size_t count, loff_t *offset)
+{
+  __int64 a2 = (__int64)buffer;
+  __int64 a3 = (__int64)count;
+  __int64 *a4 = (__int64 *)offset;
+
+  (void)file;
+  __int64 result; // x0
+  __int64 v5; // x8
+  __int64 v8; // x22
+  headset_state_show_callback_t v10; // x8
+  int v12; // w0
+  char s[32]; // [xsp+8h] [xbp-28h] BYREF
+  __int64 v14; // [xsp+10h] [xbp-20h]
+  _WORD v15[7]; // [xsp+18h] [xbp-18h] BYREF
+  __int64 v16; // [xsp+28h] [xbp-8h]
+
+  result = 0;
+  v16 = *(_QWORD *)(_ReadStatusReg(SP_EL0) + 1808);
+  v5 = *a4;
+  v14 = 0;
+  memset(v15, 0, sizeof(v15));
+  *(_QWORD *)s = 0;
+  if ( !v5 )
+  {
+    v8 = tpd_cdev;
+    v10 = *(headset_state_show_callback_t *)(tpd_cdev + 0xe78);
+    if ( v10 )
+    {
+      /* CFI check removed */
+      v10((struct ztp_device *)tpd_cdev);
+    }
+    printk(unk_3A127, "headset_state_show", *(unsigned __int8 *)(v8 + 23));
+    v12 = snprintf(s, 0x1Eu, "headset state: %u\n", *(unsigned __int8 *)(v8 + 23));
+    result = simple_read_from_buffer(a2, a3, a4, s, v12);
+  }
+  return result;
+}
