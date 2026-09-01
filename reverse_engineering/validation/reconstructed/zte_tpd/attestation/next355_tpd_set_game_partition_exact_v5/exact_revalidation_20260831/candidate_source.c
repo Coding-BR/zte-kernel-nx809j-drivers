6661a94486ce0c7309c0a6b5ba59190229d5a627
Coding-BR <Coding-BR@users.noreply.github.com>
2026-08-31 21:28:49 -0300

    attest tpd_set_game_partition exact offline parity

---
+++ reverse_engineering/validation/reconstructed/zte_tpd/attestation/next355_tpd_set_game_partition_exact_v5/exact_revalidation_20260831/candidate_source.c
@@ -0,0 +1,7 @@
+int tpd_set_game_partition(struct ztp_device *cdev, char *buffer)
+{
+  (void)cdev;
+  (void)buffer;
+  printk(unk_3A85B, "tpd_set_game_partition", "tpd_set_game_partition");
+  return 0;
+}
