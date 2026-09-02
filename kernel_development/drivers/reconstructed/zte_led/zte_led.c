251f3b935f9d4784a3ada20a8b243b97362487fb
Coding-BR <Coding-BR@users.noreply.github.com>
2026-09-02 13:34:03 -0300

    attest zte led rgb show exact

--- kernel_development/drivers/reconstructed/zte_led/zte_led.c
+++ kernel_development/drivers/reconstructed/zte_led/zte_led.c
@@ -290,6 +290,10 @@ extern ssize_t aw22xxx_imax_store(struct device *dev,
 				  struct device_attribute *attr, const char *buf,
 				  size_t count);
 #endif
+#ifdef ZTE_LED_RGB_SHOW_EXACT_ISLAND
+extern ssize_t aw22xxx_rgb_show(struct device *dev,
+				 struct device_attribute *attr, char *buf);
+#endif
 static int aw22xxx_cfg_update_wait_from_dyn_name(struct aw22xxx *aw22xxx);
 static int aw22xxx_set_cfg_run_state(u32 effect);
 static void aw22xxx_cfg_recover_update_wait(struct aw22xxx *aw22xxx);
@@ -1668,6 +1672,7 @@ static ssize_t aw22xxx_reg_store(struct device *dev, struct device_attribute *at
 }
 #endif
 
+#ifndef ZTE_LED_RGB_SHOW_EXACT_ISLAND
 static ssize_t aw22xxx_rgb_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
 	struct led_classdev *cdev = dev_get_drvdata(dev);
@@ -1680,6 +1685,7 @@ static ssize_t aw22xxx_rgb_show(struct device *dev, struct device_attribute *att
 	}
 	return len;
 }
+#endif
 
 static ssize_t aw22xxx_rgb_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
 {
