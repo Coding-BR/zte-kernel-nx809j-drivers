f31991c5fec2b8c4d2429f4331d8048421f97192
Coding-BR <Coding-BR@users.noreply.github.com>
2026-09-05 20:24:02 -0300

    Promote fingerprint pinctrl select attestation

---
+++ reverse_engineering/validation/reconstructed/zte_fingerprint/attestation/next021_zte_fingerprint_zte_goodix_pinctrl_select_exact_v1/docker/work/zte_fingerprint/cycle_2/.modules.order.cmd
@@ -0,0 +1,1 @@
+savedcmd_/work/validation/zte_fingerprint/cycle_2/modules.order := {   echo /work/validation/zte_fingerprint/cycle_2/zte_fingerprint.o; :; } > /work/validation/zte_fingerprint/cycle_2/modules.order
