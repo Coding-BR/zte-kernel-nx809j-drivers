5f0965aa4296552e1333f76b88c9726df3c34b73
Coding-BR <Coding-BR@users.noreply.github.com>
2026-09-05 22:13:24 -0300

    Promote zlog exception poll attestation

---
+++ reverse_engineering/validation/reconstructed/zlog_exception/attestation/next052_zlog_exception_zlog_poll_exact_v1/docker/work/zlog_exception/cycle_2/.modules.order.cmd
@@ -0,0 +1,1 @@
+savedcmd_/work/validation/zlog_exception/cycle_2/modules.order := {   echo /work/validation/zlog_exception/cycle_2/zlog_exception.o; :; } > /work/validation/zlog_exception/cycle_2/modules.order
---
+++ reverse_engineering/validation/reconstructed/zlog_exception/attestation/next052_zlog_exception_zlog_poll_exact_v1/docker/work/zlog_exception/cycle_2/zlog_exception.mod
@@ -0,0 +1,1 @@
+/work/validation/zlog_exception/cycle_2/zlog_exception.o
